461841a456491e9016f0f310befcfa2c7c7abb4e
Elijah Verdoorn <elijah.verdoorn@wework.com>
2019-11-29 20:37:47 -0600

    Add community link and chrono.gg link to model

--- src/main/kotlin/com/elijahverdoorn/chronogg/data/ChronoRssReader.kt
+++ src/main/kotlin/com/elijahverdoorn/chronogg/data/ChronoRssReader.kt
@@ -1,20 +1,24 @@
 package com.elijahverdoorn.chronogg.data
 
+import com.apptastic.rssreader.Item
 import com.apptastic.rssreader.RssReader
 import com.elijahverdoorn.chronogg.models.Deal
 import java.text.SimpleDateFormat
-import java.util.*
 import java.util.stream.Collectors
 
 class ChronoRssReader {
     val reader = RssReader()
+    val latestItem: Item by lazy {
+        getLatestDealItem()
+    }
 
     val latestDeal: Deal by lazy {
-        getLatestDealString().let {
+        getLatestDealString(latestItem).let {
             Deal(
                 title = parseTitle(it),
                 date = parseDate(it),
-                price = parsePrice(it)
+                price = parsePrice(it),
+                communityLink = parseLink(latestItem)
             )
         }
     }
@@ -26,9 +30,15 @@ class ChronoRssReader {
 
     private fun parseTitle(str: String) = str.substringBefore("-").trim()
 
-    private fun getLatestDealString(): String {
-        return reader.read(RSS_FEED_URL).collect(Collectors.toList()).first().title.orElseGet { "" }
+    private fun getLatestDealString(item: Item) = item.title.orElse("")
+
+    private fun getLatestDealItem(): Item {
+        return reader.read(RSS_FEED_URL)
+                .collect(Collectors.toList())
+                .first()
     }
+
+    private fun parseLink(item: Item) = item.link.orElse("")
 
     companion object {
         const val RSS_FEED_URL = "https://community.chrono.gg/c/daily-deals.rss"
--- src/main/kotlin/com/elijahverdoorn/chronogg/models/Deal.kt
+++ src/main/kotlin/com/elijahverdoorn/chronogg/models/Deal.kt
@@ -2,9 +2,13 @@ package com.elijahverdoorn.chronogg.models
 
 import java.util.*
 
+const val CHRONO_GG_URL = "https://chrono.gg"
+
 data class Deal(
     val title: String, // The title of the game on sale
     val date: Date, // The full date of the sale
     val price: Float, // The price of the game for sale, in USD
-    val timestamp: Date = Date()// The time that this record was created.
+    val communityLink: String, // A link to the chrono.gg community page for this deal
+    val link: String = CHRONO_GG_URL, // A link to chrono.gg main page
+    val timestamp: Date = Date() // The time that this record was created.
 )
